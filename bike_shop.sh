a2b8a9427865936b6758042975319a82bd056e84
wreichle87 <168371665+wreichle87@users.noreply.github.com>
2024-08-31 15:42:29 -0400

    Create bike_shop.sh

---
+++ bike_shop.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+echo -e '\n~~~~~ Bike Rental Shop ~~~~~\n'
+MAIN_MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+  echo How may I help you?
+  echo
+  echo 1. Rent a bike
+  echo 2. Return a bike
+  echo 3. Exit
+  read MAIN_MENU_SELECTION
+  case $MAIN_MENU_SELECTION in
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU "Please enter a valid option." ;;
+  esac
+}
+RENT_MENU() {
+  #get available bikes
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id,type,size FROM bikes WHERE available=true ORDER BY bike_id;")
+  #if no bikes available
+  if [[ -z $AVAILABLE_BIKES ]]
+  then
+    #send to main menu
+    MAIN_MENU "Sorry, we don't have any bikes available right now."
+  else
+    #display available bikes
+    echo -e "\nHere are the bikes we have available:"
+    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+    do
+      echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+    done
+    #ask for bike to rent
+    echo -e "\nWhich one would you like to rent?"
+    read BIKE_ID_TO_RENT
+    #if input is not a number
+    #send to main menu
+  fi
+}
+RETURN_MENU() {
+  echo Return Menu
+}
+EXIT() {
+  echo -e "\nThank you for stopping in.\n"
+}
+MAIN_MENU
